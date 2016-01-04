54c1f9bf02a6e62605cd4adabbc920efa0d2168d
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-01-04 17:45:55 +0000

    Adding a custom Arr::dot() Helper

---
+++ src/Helpers/Arr.php
@@ -0,0 +1,36 @@
+<?php namespace Arcanesoft\Settings\Helpers;
+
+/**
+ * Class     Arr
+ *
+ * @package  Arcanesoft\Settings\Helpers
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class Arr extends \Illuminate\Support\Arr
+{
+    /**
+     * Flatten a multi-dimensional associative array with dots.
+     *
+     * @param  array   $array
+     * @param  string  $prepend
+     *
+     * @return array
+     */
+    public static function dot($array, $prepend = '')
+    {
+        $results = [];
+
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                if (self::isAssoc($value)) {
+                    $results = array_merge($results, self::dot($value, $prepend.$key.'.'));
+                }
+            }
+            else {
+                $results[$prepend.$key] = $value;
+            }
+        }
+
+        return $results;
+    }
+}
